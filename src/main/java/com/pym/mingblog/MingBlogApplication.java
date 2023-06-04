f56bbb526a84e5a816b96abb505bc8ce05dced35
R5zzz <1602315416@qq.com>
2023-06-05 01:01:56 +0800

    initial Project

---
+++ src/main/java/com/pym/mingblog/MingBlogApplication.java
@@ -0,0 +1,13 @@
+package com.pym.mingblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MingBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pym/mingblog/MingBlogApplicationTests.java
@@ -0,0 +1,21 @@
+package com.pym.mingblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Scanner;
+
+@SpringBootTest
+class MingBlogApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
+
+
+
+
