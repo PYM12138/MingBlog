eea775c1928120a66030be028d0082a7bdc65604
R5zzz <1602315416@qq.com>
2023-07-29 17:03:50 +0800

    初步完成所有的前端页面编写

--- src/main/java/com/pym/mingblog/controller/DirectLinkController.java
+++ src/main/java/com/pym/mingblog/controller/DirectLinkController.java
@@ -16,41 +16,105 @@ public class DirectLinkController {
     public String index() {
         return "index";
     }
-
+    //文章页
     @GetMapping("/article")
     public String article() {
         return "article";
     }
 
+    //归档页
     @GetMapping("/archive")
     public String archive() {
         return "archive";
     }
 
+    //学习页
     @GetMapping("/learn")
     public String learn() {
         return "learn";
     }
 
+    //更新日记
     @GetMapping("/updateRecord")
     public String updateRecord() {
         return "updateRecord";
     }
 
+    //关于我
     @GetMapping("/aboutMe")
     public String aboutMe() {
         return "aboutMe";
     }
 
+    //登录注册找回密码
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/recoverPassword")
+    public String recoverPassword() {
+        return "recoverPassword";
+    }
+
+    @GetMapping("/forgotPassword")
+    public String forgotPassword() {
+        return "forgotPassword";
+    }
+
+
     //后台
+    //首页
     @GetMapping("/adminIndex")
-    public String indexAdmin() {
+    public String adminIndex() {
         return "admin/index";
     }
 
+    //文章编辑页
     @GetMapping("/adminEdit")
-    public String editAdmin() {
+    public String adminEdit() {
         return "admin/edit";
+    }
+
+    //文章管理页
+    @GetMapping("/adminMA")
+    public String adminManagerArticle() {
+        return "admin/managerArticle";
+    }
+
+    //文章操作记录
+    @GetMapping("/adminAL")
+    public String adminArticleLog() {
+        return "admin/articleLog";
+    }
+
+    //用户管理页
+    @GetMapping("/adminUser")
+    public String adminUser() {
+        return "admin/managerUser";
+    }
+
+    //用户详情页
+    @GetMapping("/userPage")
+    public String userPage() {
+        return "admin/userPage";
+    }
+
+    //评论审核
+    @GetMapping("/newMessage")
+    public String newMessage() {
+        return "admin/newMessage";
+    }
+
+    //历史评论
+    @GetMapping("/historyMessage")
+    public String historyMessage() {
+        return "admin/historyMessage";
     }
 
 
